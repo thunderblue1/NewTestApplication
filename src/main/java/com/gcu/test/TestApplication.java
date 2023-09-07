1ce594b1ed11ad9b0bbaccf2fb998d8ee220c96f
John Keen <blueisgoodtoo@gmail.com>
2023-09-07 01:12:26 -0700

    Revert

--- src/main/java/com/gcu/test/TestApplication.java
+++ src/main/java/com/gcu/test/TestApplication.java
@@ -2,10 +2,9 @@ package com.gcu.test;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class TestApplication extends SpringBootServletInitializer {
+public class TestApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TestApplication.class, args);
