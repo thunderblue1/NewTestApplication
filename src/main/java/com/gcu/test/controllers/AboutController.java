29e002be91ac6bcabb98e22a4f54fb3af858ce69
John Keen <blueisgoodtoo@gmail.com>
2023-10-03 15:19:20 -0700

    Project update

--- src/main/java/com/gcu/test/controllers/AboutController.java
+++ src/main/java/com/gcu/test/controllers/AboutController.java
@@ -1,12 +1,17 @@
 package com.gcu.test.controllers;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class AboutController {
+	private static final Logger logger = LoggerFactory.getLogger(AboutController.class);
+	
 	@RequestMapping({"/","about"})
 	public String index() {
+		logger.info("The about page is logged.");
 		return "about.html";
 	}
 }
