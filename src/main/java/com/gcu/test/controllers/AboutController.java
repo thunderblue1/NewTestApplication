e13bd2a036242eba1d8708c2de21899b7b797eee
John Keen <blueisgoodtoo@gmail.com>
2023-10-10 22:50:43 +0000

    Testing Cloud9 Git features

--- src/main/java/com/gcu/test/controllers/AboutController.java
+++ src/main/java/com/gcu/test/controllers/AboutController.java
@@ -1,6 +1,6 @@
 package com.gcu.test.controllers;
 
-import org.slf4j.Logger;
+
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
